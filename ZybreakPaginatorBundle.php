272828f74c4054cb66870d096872693e61a9d0e9
Robert Knutsson <zybreak@gmail.com>
2011-09-05 15:51:10 +0200

    added missing file

---
+++ ZybreakPaginatorBundle.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * (c) Thibault Duplessis <thibault.duplessis@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Zybreak\PaginatorBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class ZybreakPaginatorBundle extends Bundle
+{
+}
+
