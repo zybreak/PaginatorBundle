2b7c329084e5036c6fd3dd0a266150ff96a5202e
www-data <www-data@rehifi.se>
2011-10-06 16:01:53 +0200

    Added sortable link

--- Templating/Helper/PaginationHelper.php
+++ Templating/Helper/PaginationHelper.php
@@ -92,6 +92,58 @@ class PaginationHelper extends Helper
     }
 
     /**
+     * Create a sort url for the field named $title
+     * and identified by $key which consists of
+     * alias and field. $options holds all link
+     * parameters like "alt, class" and so on.
+     *
+     * $key example: "article.title"
+     *
+     * @param Zend\Paginator\Paginator $paginator
+     * @param string $title
+     * @param string $key
+     * @param array $options
+     * @param array $params
+     * @param string $route
+     * @return string
+     */
+    public function sortable(Paginator $paginator, $title, $key, $options = array(), $params = array(), $route = null)
+    {
+        $alias = $this->getAlias($paginator);
+        $options = array_merge(array(
+            'absolute' => false
+        ), $options);
+
+        if (null === $route) {
+            $route = $this->route;
+        }
+        $params = array_merge($this->params, $params);
+        $direction = isset($options[$alias.'direction']) ? $options[$alias.'direction'] : 'asc';
+
+        $sorted = isset($params[$alias.'sort']) && $params[$alias.'sort'] == $key;
+        if ($sorted) {
+            $direction = $params[$alias.'direction'];
+            $direction = (strtolower($direction) == 'asc') ? 'desc' : 'asc';
+            $class = $direction == 'asc' ? 'desc' : 'asc';
+            if (isset($options['class'])) {
+                $options['class'] .= ' ' . $class;
+            } else {
+                $options['class'] = $class;
+            }
+        } else {
+            $options['class'] = 'sortable';
+        }
+        if (is_array($title) && array_key_exists($direction, $title)) {
+            $title = $title[$direction];
+        }
+        $params = array_merge(
+            $params,
+            array($alias.'sort' => $key, $alias.'direction' => $direction)
+        );
+        return $this->buildLink($params, $route, $this->translator->trans($title), $options);
+    }
+
+    /**
      * Renders a pagination control, for a $paginator given.
      * Optionaly $template and $style can be specified to
      * override default from configuration.
@@ -144,5 +196,28 @@ class PaginationHelper extends Helper
     public function getName()
     {
         return 'pagination';
+    }
+
+    /**
+     * Build a HTML link. $options holds all link parameters
+     * like "alt, class" and so on. $title can also be an image
+     * if required.
+     *
+     * @param array $params - url query params
+     * @param string $route
+     * @param string $title
+     * @param array $options
+     * @return string
+     */
+    private function buildLink($params, $route, $title, $options = array())
+    {
+        $options['href'] = $this->routerHelper->generate($route, $params, $options['absolute']);
+        unset($options['absolute']);
+
+        if (!isset($options['title'])) {
+            $options['title'] = $title;
+        }
+
+        return $this->engine->render('ZybreakPaginatorBundle:Pagination:sortable_link.html.twig', array('options' => $options, 'title' => $title));
     }
 }
